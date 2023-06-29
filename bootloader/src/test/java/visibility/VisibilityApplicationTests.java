0fa4a7008e8eb098e07cb439262caa19ad0eba25
Javier Pazó Otero <javi.pazo@gmail.com>
2023-06-29 10:42:20 +0200

    first commit

---
+++ bootloader/src/test/java/visibility/VisibilityApplicationTests.java
@@ -0,0 +1,13 @@
+package visibility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisibilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
